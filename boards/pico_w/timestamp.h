d6797fc9e224a893d92d1575448ce883eb29de58
Mitch Cairns <mitch.cairns@handheldlegend.com>
2025-02-17 21:22:19 -0800

    SDK BT Changes

--- boards/pico_w/timestamp.h
+++ boards/pico_w/timestamp.h
@@ -2,6 +2,6 @@
 #define BUILD_TIMESTAMP_H
 
 #include <stdint.h>
-#define BUILD_TIMESTAMP ((uint32_t)1739517694)
+#define BUILD_TIMESTAMP ((uint32_t)1739854955)
 
 #endif /* BUILD_TIMESTAMP_H */
--- boards/super_gamepad/timestamp.h
+++ boards/super_gamepad/timestamp.h
@@ -2,6 +2,6 @@
 #define BUILD_TIMESTAMP_H
 
 #include <stdint.h>
-#define BUILD_TIMESTAMP ((uint32_t)1739515797)
+#define BUILD_TIMESTAMP ((uint32_t)1739842086)
 
 #endif /* BUILD_TIMESTAMP_H */
